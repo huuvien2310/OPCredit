3a0468cdcb89ff89e92ed02c75f06f857703dbb3
Vien Hua <vhua@worcester.edu>
2021-01-23 14:02:28 -0500

    Login page commit

---
+++ app/src/main/java/com/example/hackathonproject/HomePage.java
@@ -0,0 +1,14 @@
+package com.example.hackathonproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomePage extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.homepage_activity);
+    }
+}
--- app/src/main/java/com/example/hackathonproject/MainActivity.java
+++ app/src/main/java/com/example/hackathonproject/MainActivity.java
@@ -12,45 +12,80 @@ import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    /* Define the UI elements */
+    private EditText eName;
+    private EditText ePassword;
+    private Button eLogin;
+
+    String userName = "Admin";
+    String userPassword = "123456";
+
+    boolean isValid = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        /*FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        /* Bind the XML views to Java Code Elements */
+        eName = findViewById(R.id.etName);
+        ePassword = findViewById(R.id.etPassword);
+        eLogin = findViewById(R.id.btnLogin);
+
+        /* Describe the logic when the login button is clicked */
+        eLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                /* Obtain user inputs */
+                String inputName = eName.getText().toString();
+                String inputPassword = ePassword.getText().toString();
+
+                /* Check if the user inputs are empty */
+                if(inputName.isEmpty() || inputPassword.isEmpty())
+                {
+                    /* Display a message toast to user to enter the details */
+                    Toast.makeText(MainActivity.this, "Please enter name and password!", Toast.LENGTH_SHORT).show();
+
+                }else {
+
+                    /* Validate the user inputs */
+                    isValid = validate(inputName, inputPassword);
+                    /* Validate the user inputs */
+
+                    /* If not valid */
+                    if (!isValid) {
+                        Toast.makeText(MainActivity.this, "Incorrect credentials, please try again!", Toast.LENGTH_SHORT).show();
+                    }
+                    /* If valid */
+                    else {
+                        Toast.makeText(MainActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+                        /* Allow the user into the app by going into the next activity */
+                        Intent intent = new Intent(MainActivity.this, HomePage.class);
+                        startActivity(intent);
+                    }
+                }
             }
-        });*/
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+    /* Validate the credentials */
+    private boolean validate(String name, String password)
+    {
+        /* Check the credentials */
+        if(name.equals(userName) && password.equals(userPassword)){
             return true;
         }
-
-        return super.onOptionsItemSelected(item);
+        return false;
     }
 }
