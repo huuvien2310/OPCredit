2a6da90d45b8e0005f610e939c2c2876f5f613a5
Vien Hua <vhua@worcester.edu>
2021-01-23 09:29:14 -0500

    fragment_first commit

--- app/src/main/java/com/example/hackathonproject/MainActivity.java
+++ app/src/main/java/com/example/hackathonproject/MainActivity.java
@@ -22,16 +22,16 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
+        /*FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
     }
-//sth
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
